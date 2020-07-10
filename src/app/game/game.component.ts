646eaec1f0338f72726e495f47bf29b6e0aeacaa
brian davidson <brian.davidson.1981@gmail.com>
2020-07-10 16:13:22 -0400

    5 - created basic phaser game

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,4 +1,12 @@
+declare var Phaser;
 import { Component, OnInit } from '@angular/core';
+
+// phaser vars
+let game;
+let player;
+let bg;
+let cursors;
+let that;
 
 @Component({
   selector: 'app-game',
@@ -7,9 +15,66 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GameComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+    game = new Phaser.Game(window.innerWidth, window.innerHeight, Phaser.AUTO, 'game', {
+      preload: this.preload,
+      create: this.create,
+      update: this.update,
+      render: this.render
+    });
+    that = Object.create(this.constructor.prototype);
+  }
 
   ngOnInit() {
   }
+
+  preload() {
+    game.load.spritesheet('character', 'assets/images/character24x24x8.png', 24, 24);
+    game.load.image('bg', 'assets/images/bg.png');
+  }
+
+  create() {
+    game.physics.startSystem(Phaser.Physics.ARCADE);
+
+    // background sprite
+    bg = game.add.tileSprite(0, 0, window.innerWidth, window.innerHeight, 'bg');
+
+    // player sprite
+    player = game.add.sprite(window.innerWidth / 2, window.innerHeight / 2, 'character', 1);
+    player.animations.add('up', [0, 1], 2, true);
+    player.animations.add('right', [2, 3], 2, true);
+    player.animations.add('left', [4, 5], 2, true);
+    player.animations.add('down', [6, 7], 2, true);
+    player.play('down');
+    player.anchor.setTo(0.5, 0.5);
+    game.physics.enable(player, Phaser.Physics.ARCADE);
+
+    // phaser input
+    cursors = game.input.keyboard.createCursorKeys();
+  }
+
+  update() {
+    if (player.alive) {
+      player.body.velocity.setTo(0, 0);
+      if (cursors.left.isDown) {
+        player.play('left');
+        player.body.velocity.x += -200;
+      }
+      if (cursors.right.isDown) {
+        player.play('right')
+        player.body.velocity.x += 200;
+      }
+      if (cursors.up.isDown) {
+        player.play('up')
+        player.body.velocity.y += -200;
+      }
+      if (cursors.down.isDown) {
+        player.play('down')
+        player.body.velocity.y += 200;
+      }
+    }
+  }
+
+  render() {}
 
 }
