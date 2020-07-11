ea8f902a1f56b54799d021d57992456c201aef17
shelbykauth <shelbykauth@gmail.com>
2020-07-10 20:43:32 -0400

    Convert code for Phaser2 to Phaser3
    - Prettify.  Sorry!  Unavoidable
    - Remove global variables (put them in the DemoScene class)
    - Move Game/Scene methods to DemoScene class (which gets passed in in place of {methodName:method})
    - Reference `this` (the scene) instead of `game`
    - Add a line from the tutorial to prevent player from leaving screen
    - Animations belong to the game, rather than the sprite

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,80 +1,107 @@
-declare var Phaser;
-import { Component, OnInit } from '@angular/core';
-
-// phaser vars
-let game;
-let player;
-let bg;
-let cursors;
-let that;
+// declare var Phaser;
+import { Component, OnInit } from "@angular/core";
+import "phaser";
 
 @Component({
-  selector: 'app-game',
-  templateUrl: './game.component.html',
-  styleUrls: ['./game.component.sass']
+  selector: "app-game",
+  templateUrl: "./game.component.html",
+  styleUrls: ["./game.component.sass"],
 })
 export class GameComponent implements OnInit {
-
   constructor() {
-    game = new Phaser.Game(window.innerWidth, window.innerHeight, Phaser.AUTO, 'game', {
-      preload: this.preload,
-      create: this.create,
-      update: this.update,
-      render: this.render
+    let game = new Phaser.Game({
+      width: window.innerWidth,
+      height: window.innerHeight,
+      type: Phaser.AUTO,
+      physics: {
+        default: "arcade",
+        arcade: {
+          gravity: { y: 0, x: 0 },
+        },
+      },
+      // name: "game",
+      scene: DemoScene,
     });
-    that = Object.create(this.constructor.prototype);
   }
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
+}
 
+class DemoScene extends Phaser.Scene {
+  _player: Phaser.Physics.Arcade.Sprite;
+  _cursors: Phaser.Types.Input.Keyboard.CursorKeys;
   preload() {
-    game.load.spritesheet('character', 'assets/images/character24x24x8.png', 24, 24);
-    game.load.image('bg', 'assets/images/bg.png');
+    this.load.spritesheet("character", "assets/images/character24x24x8.png", {
+      frameWidth: 24,
+      frameHeight: 24,
+    });
+    this.load.image("bg", "assets/images/bg.png");
   }
 
   create() {
-    game.physics.startSystem(Phaser.Physics.ARCADE);
-
     // background sprite
-    bg = game.add.tileSprite(0, 0, window.innerWidth, window.innerHeight, 'bg');
+    this.add.tileSprite(
+      window.innerWidth / 2,
+      window.innerHeight / 2,
+      window.innerWidth,
+      window.innerHeight,
+      "bg"
+    );
 
     // player sprite
-    player = game.add.sprite(window.innerWidth / 2, window.innerHeight / 2, 'character', 1);
-    player.animations.add('up', [0, 1], 2, true);
-    player.animations.add('right', [2, 3], 2, true);
-    player.animations.add('left', [4, 5], 2, true);
-    player.animations.add('down', [6, 7], 2, true);
-    player.play('down');
-    player.anchor.setTo(0.5, 0.5);
-    game.physics.enable(player, Phaser.Physics.ARCADE);
+    this._player = this.physics.add.sprite(
+      window.innerWidth / 2,
+      window.innerHeight / 2,
+      "character",
+      1
+    );
+    this._player.setBounce(0.2);
+    this._player.setCollideWorldBounds(true);
 
-    // phaser input
-    cursors = game.input.keyboard.createCursorKeys();
+    // animations
+    [
+      { key: "up", start: 0, end: 1 },
+      { key: "right", start: 2, end: 3 },
+      { key: "left", start: 4, end: 5 },
+      { key: "down", start: 6, end: 7 },
+    ].forEach((frame) => {
+      this.anims.create({
+        key: frame.key,
+        frames: this.anims.generateFrameNumbers("character", {
+          start: frame.start,
+          end: frame.end,
+        }),
+        frameRate: 2,
+        repeat: -1,
+      });
+    });
+    this._player.play("down");
+    this._player.originX = 0.5;
+    this._player.originY = 0.5;
+    this._cursors = this.input.keyboard.createCursorKeys();
   }
 
   update() {
-    if (player.alive) {
-      player.body.velocity.setTo(0, 0);
-      if (cursors.left.isDown) {
-        player.play('left');
-        player.body.velocity.x += -200;
+    if (this._player.active) {
+      this._player.body.velocity.setTo(0, 0);
+      if (this._cursors.left.isDown) {
+        this._player.play("left");
+        this._player.body.velocity.x += -200;
       }
-      if (cursors.right.isDown) {
-        player.play('right')
-        player.body.velocity.x += 200;
+      if (this._cursors.right.isDown) {
+        this._player.play("right");
+        this._player.body.velocity.x += 200;
       }
-      if (cursors.up.isDown) {
-        player.play('up')
-        player.body.velocity.y += -200;
+      if (this._cursors.up.isDown) {
+        this._player.play("up");
+        this._player.body.velocity.y += -200;
       }
-      if (cursors.down.isDown) {
-        player.play('down')
-        player.body.velocity.y += 200;
+      if (this._cursors.down.isDown) {
+        this._player.play("down");
+        this._player.body.velocity.y += 200;
       }
     }
   }
 
   render() {}
-
 }
